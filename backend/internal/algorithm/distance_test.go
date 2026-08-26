a648976152a544e5423f8f5a28629d86ec0e0b76
Lewis <231671604+lbram09@users.noreply.github.com>
2026-08-26 16:51:59 +0100

    add matrix tests and move testing utils over to separate file

--- backend/internal/algorithm/distance_test.go
+++ backend/internal/algorithm/distance_test.go
@@ -2,10 +2,8 @@ package algorithm
 
 import (
 	"math"
-	"math/rand/v2"
 	"testing"
 
-	"github.com/bramlew/NEA/backend/internal/models"
 	"github.com/pymaxion/geographiclib-go/v2/geodesic"
 )
 
@@ -14,16 +12,16 @@ func TestInit(t *testing.T) {
 	const N int = 10 // No. of times to randomly execute the tests
 	for i := 0; i < N; i++ {
 		// Test some valid coordinates
-		_, err := GreatCircleDistance(randValidCoords(), randValidCoords())
+		_, err := GreatCircleDistance(RandValidCoords(), RandValidCoords())
 		if err != nil {
 			t.Errorf("error calculating great circle distance: %v", err)
 		}
 	}
 	for i := 0; i < N; i++ {
 		// Test some invalid coordinates
-		_, err := GreatCircleDistance(randInvalidCoords(), randInvalidCoords())
+		_, err := GreatCircleDistance(RandInvalidCoords(), RandInvalidCoords())
 		if err == nil {
-			t.Errorf("expected error for invalid coordinates, got none")
+			t.Error("expected error for invalid coordinates, got none")
 		}
 	}
 }
@@ -32,7 +30,7 @@ func TestAccuracy(t *testing.T) {
 	// Test the accuracy of the great circle distance calculation algorithm with random valid and invalid sets of coordinates
 	const N int = 10000 // No. of times to randomly execute the test
 	for i := 0; i < N; i++ {
-		origin, dest := randValidCoords(), randValidCoords()
+		origin, dest := RandValidCoords(), RandValidCoords()
 		d, err := GreatCircleDistance(origin, dest)
 		actD := geodesic.WGS84.Inverse(origin.Lat, origin.Lon, dest.Lat, dest.Lon).S12 / 1000
 		if err != nil {
@@ -42,28 +40,4 @@ func TestAccuracy(t *testing.T) {
 			t.Errorf("calculated distance is not within expected error range, expected maximum 0.5%% error, got %.2f%%", math.Abs(d-actD)/actD*100)
 		}
 	}
-}
-
-func randValidCoords() models.Coords {
-	// Generate a set of random valid coordinates
-	return models.Coords{
-		Lon: (rand.Float64() - 0.5) * 360,
-		Lat: (rand.Float64() - 0.5) * 180,
-	}
-}
-
-func randInvalidCoords() models.Coords {
-	// Generate a set of random invalid coordinates
-	return models.Coords{
-		Lon: randSign() * (rand.Float64()*180 + 180),
-		Lat: randSign() * (rand.Float64()*90 + 90),
-	}
-}
-
-func randSign() float64 {
-	// Generates either positive 1 or negative 1, by using the remainder when dividing by two of a random unsigned 32-bit integer, hence 2 equally probable outputs
-	if rand.Uint32()%2 == 0 {
-		return 1
-	}
-	return -1
 }
--- backend/internal/algorithm/matrix_test.go
+++ backend/internal/algorithm/matrix_test.go
@@ -1,1 +1,38 @@
 package algorithm
+
+import (
+	"math"
+	"testing"
+
+	"github.com/bramlew/NEA/backend/internal/models"
+)
+
+func TestMatrixValidity(t *testing.T) {
+	// Test that the constructed matrix has correct values
+	const N int = 10 // No. of random locations to generate
+	locations := make([]models.Coords, N)
+	for i := 0; i < N; i++ {
+		locations[i] = RandValidCoords()
+	}
+
+	m, err := ConstructMatrix(locations)
+	if err != nil {
+		t.Errorf("error constructing matrix: %v", err)
+	}
+	for i, origin := range locations {
+		for j, dest := range locations {
+			matrixDistance := m.Matrix[LookupIndex(i, j, m.Cols)].Distance
+			if i != j {
+				d, err := GreatCircleDistance(origin, dest)
+				if err != nil {
+					t.Errorf("error calculating great circle distance: %v", err)
+				}
+				if d != matrixDistance {
+					t.Errorf("expected distance %v at index [%v, %v], got %v", d, i, j, matrixDistance)
+				}
+			} else if !math.IsInf(matrixDistance, 1) {
+				t.Errorf("expected infinite distance at index [%v, %v], got %v", i, j, matrixDistance)
+			}
+		}
+	}
+}
