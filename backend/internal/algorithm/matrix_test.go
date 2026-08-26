68524c843a452d8701987e9840337da713e7f790
Lewis <231671604+lbram09@users.noreply.github.com>
2026-08-26 16:51:59 +0100

    add matrix tests and move testing utils over to separate file

--- backend/internal/algorithm/matrix_test.go
+++ backend/internal/algorithm/matrix_test.go
@@ -1,1 +1,33 @@
 package algorithm
+
+import (
+	"testing"
+
+	"github.com/bramlew/NEA/backend/internal/models"
+)
+
+func TestMatrixValidity(t *testing.T) {
+	// Test that the constructed matrix has correct values
+	const N int = 10 // No. of random locations to generate
+	locations := make([]models.Coords, N)
+	for i := 0; i < N; i++ {
+		locations[i] = RandValidCoords()
+	}
+
+	m, err := ConstructMatrix(locations)
+	if err != nil {
+		t.Errorf("error constructing matrix: %v", err)
+	}
+	for i, origin := range locations {
+		for j, dest := range locations {
+			index := LookupIndex(i, j, m.Cols)
+			d, err := GreatCircleDistance(origin, dest)
+			if err != nil {
+				t.Errorf("error calculating great circle distance: %v", err)
+			}
+			if d != m.Matrix[index].Distance {
+				t.Errorf("expected value %v at index [%v, %v], got %v", d, i, j, m.Matrix[index].Distance)
+			}
+		}
+	}
+}
---
+++ backend/internal/algorithm/utils_test.go
@@ -0,0 +1,31 @@
+package algorithm
+
+import (
+	"math/rand/v2"
+
+	"github.com/bramlew/NEA/backend/internal/models"
+)
+
+func RandValidCoords() models.Coords {
+	// Generate a set of random valid coordinates
+	return models.Coords{
+		Lon: (rand.Float64() - 0.5) * 360,
+		Lat: (rand.Float64() - 0.5) * 180,
+	}
+}
+
+func RandInvalidCoords() models.Coords {
+	// Generate a set of random invalid coordinates
+	return models.Coords{
+		Lon: RandSign() * (rand.Float64()*180 + 180),
+		Lat: RandSign() * (rand.Float64()*90 + 90),
+	}
+}
+
+func RandSign() float64 {
+	// Generates either positive 1 or negative 1, by using the remainder when dividing by two of a random unsigned 32-bit integer, hence 2 equally probable outputs
+	if rand.Uint32()%2 == 0 {
+		return 1
+	}
+	return -1
+}
