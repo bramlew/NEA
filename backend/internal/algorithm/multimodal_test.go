0396cdb488f77311c1bb9cd3b1dad84e451dca3c
Lewis <231671604+lbram09@users.noreply.github.com>
2026-09-21 10:47:41 +0100

    test api thingy and res of mm json file

--- backend/internal/algorithm/multimodal_test.go
+++ backend/internal/algorithm/multimodal_test.go
@@ -1,6 +1,13 @@
 package algorithm
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/bramlew/NEA/backend/internal/models"
+	"github.com/goccy/go-json"
+)
 
 func TestMultimodal(t *testing.T) {
 	// Test the multimodal matrix construction algorithm
@@ -18,5 +25,32 @@ func TestMultimodal(t *testing.T) {
 		if multimodalLeg.IsRoad && multimodalLeg.Distance == standard.Matrix[i].Distance {
 			t.Errorf("multimodal road distance same as standard distance: %.2f", multimodalLeg.Distance)
 		}
+	}
+}
+
+func TestAPI(t *testing.T) {
+	// Test the API for errors whilst running multimodal algorithm
+	jsonFile, err := os.Open("..\\..\\..\\json\\api\\first\\request.json")
+	if err != nil {
+		t.Fatalf("unable to open json file: %v", err)
+	}
+	defer jsonFile.Close()
+	jsonBody, err := io.ReadAll(jsonFile)
+	if err != nil {
+		t.Fatalf("unable to read json file: %v", err)
+	}
+	var req models.Request
+	err = json.Unmarshal(jsonBody, &req)
+	if err != nil {
+		t.Fatalf("unable to unmarshal json: %v", err)
+	}
+	locations := req.Locations
+	mat, err := ConstructMultimodalMatrix(locations)
+	if err != nil {
+		t.Errorf("error constructing multimodal matrix: %v", err)
+	}
+	_, err = MultiTSP(mat)
+	if err != nil {
+		t.Errorf("error running tsp algorithms: %v", err)
 	}
 }
